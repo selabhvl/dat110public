740863896cc49e754c958b3d3473315a8cd84c0f
Lars Kristensen <lmkr@hib.no>
2021-01-15 14:40:19 +0100

    Added basic UDP Socket example

---
+++ week3/basicsocket/src/no/hvl/dat110/tcpsockets/TCPReceiver.java
@@ -0,0 +1,10 @@
+package no.hvl.dat110.tcpsockets;
+
+public class TCPReceiver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week3/basicsocket/src/no/hvl/dat110/tcpsockets/TCPSender.java
@@ -0,0 +1,10 @@
+package no.hvl.dat110.tcpsockets;
+
+public class TCPSender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
